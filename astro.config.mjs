b5ddd124e78bf52966f4bf86c69311ab462cd80b
attilanagy23 <168219533+attilanagy23@users.noreply.github.com>
2026-05-04 19:48:31 +0200

    Fix canonical URL hyphenation (expert-flow-start-2-0)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://expert-flow-start-2.vercel.app',
+  site: 'https://expert-flow-start-2-0.vercel.app',
   trailingSlash: 'never',
 });
